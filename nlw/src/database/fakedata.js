914a9afb0723d4af1118a2ca58e8bbfa674090d1
Enzo Kozoroski Giovanini <enzogiovanini@gmail.com>
2020-10-17 20:05:12 -0400

    Adicionando aula 4.

---
+++ nlw/src/database/fakedata.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-21.4820604",
+        lng: "-56.1533031",
+        name: "Lar das meninas",
+        description: "Presta assistência a crinças de 06 à 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1598252976330-b8a1461d47a7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1598135753053-2e3075f90763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2,
+        lat: "-21.4820604",
+        lng: "-56.1633031",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crinças de 06 à 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1598252976330-b8a1461d47a7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1598135753053-2e3075f90763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
